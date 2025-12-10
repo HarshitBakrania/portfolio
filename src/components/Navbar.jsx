16e8aec17c4ba1563310ec08c54a05dead96e473
Harshit Bakrania <harshit.bakrania@gmail.com>
2025-12-11 00:55:17 +0530

    finished NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { navLinks } from "@constants"
+import dayjs from "dayjs"
+import { navIcons, navLinks } from "@constants"
 
 export const Navbar = () =>{
     return (
@@ -14,6 +15,17 @@ export const Navbar = () =>{
                         </li>
                     ))}
                 </ul>
+            </div>
+            
+            <div>
+                <ul>
+                    {navIcons.map((icons) =>(
+                        <li key={icons.id}>
+                            <img src={icons.img} alt={`icon-${icons.id}`}></img>
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format("ddd MMM D h:mm a")}</time>
             </div>
         </nav>
     )
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,10 @@ export const navLinks = [
     { id: 2, name: "Contact"},
     { id:3, name: "Projects"}
 ];
+
+export const navIcons = [
+    { id: 1, img: "/icons/wifi.svg"},
+    { id: 2, img: "/icons/search.svg"},
+    { id: 3, img: "/icons/user.svg"},
+    { id: 4, img: "/icons/mode.svg"}
+]

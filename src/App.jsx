12d8cd0fc9d1829e675e8fc55a22ee9472e90669
Harshit Bakrania <harshit.bakrania@gmail.com>
2025-12-10 23:20:22 +0530

    project setup & tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () =>{
-  return <h1>Portfolio</h1>
+  return <h1 className="text-3xl">Portfolio</h1>
 };
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirName } from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@components" : resolve(dirName(fileURLToPath(import.meta.url)), 'components'),
+      "@constants" : resolve(dirName(fileURLToPath(import.meta.url)), 'constants'),
+      "@store" : resolve(dirName(fileURLToPath(import.meta.url)), 'store'),
+      "@hoc" : resolve(dirName(fileURLToPath(import.meta.url)), 'hoc'),
+      "@windows" : resolve(dirName(fileURLToPath(import.meta.url)), 'windows'),
+    }
+  }
 })

5899f8f7ce0e13b69d79e8453d1b4c4dae67132c
Harshit Bakrania <harshit.bakrania@gmail.com>
2025-12-17 23:28:20 +0530

    window animations added & terminal window completed

--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -1,11 +1,42 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { useWindowStore } from "@store/window";
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
+import { Draggable } from "gsap/Draggable";
 
 export const WindowWrapper = (Component, windowKey) => {
   const Wrapped = (props) => {
     const { focusWindow, windows } = useWindowStore();
     const { isOpen, zIndex } = windows[windowKey];
     const windowRef = useRef(null);
+
+    useGSAP(() => {
+      const el = windowRef.current;
+      if (!el || !isOpen) return;
+
+      el.style.display = "block";
+
+      gsap.fromTo(
+        el,
+        { scale: 0.8, opacity: 0, y: 40 },
+        { scale: 1, opacity: 1, y: 0, duration: 0.5, ease: "power3.out" }
+      );
+    }, [isOpen]);
+
+    useGSAP(() =>{
+      const el = windowRef.current;
+      if (!el) return;
+
+      const [instance] = Draggable.create(el, { onPress: () => focusWindow(windowKey)});
+      
+      return () => instance.kill();
+    }, [])
+
+    useLayoutEffect(() => {
+      const el = windowRef.current;
+      if (!el) return;
+      el.style.display = isOpen ? "block" : "none";
+    }, [isOpen]);
 
     return (
       <section
@@ -14,12 +45,14 @@ export const WindowWrapper = (Component, windowKey) => {
         style={{ zIndex }}
         className="absolute"
       >
-        <Component {...props}/>
+        <Component {...props} />
       </section>
     );
   };
 
-  Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"})`;
+  Wrapped.displayName = `WindowWrapper(${
+    Component.displayName || Component.name || "Component"
+  })`;
 
   return Wrapped;
 };
--- src/windows/Terminal.jsx
+++ src/windows/Terminal.jsx
@@ -1,3 +1,4 @@
+import { WindowControls } from "@components/WindowControls";
 import { techStack } from "@constants";
 import { WindowWrapper } from "@hoc/WindowWrapper";
 import { Check, Flag } from "lucide-react";
@@ -6,14 +7,14 @@ const Terminal = () => {
   return (
     <>
       <div id="window-header">
-        <p>Window Controls</p>
+        <WindowControls target="terminal" />
         <h2>Tech stack</h2>
       </div>
 
       <div className="techstack">
         <p>
-            <span className="font-bold">@harshit % </span>
-            show tech stack
+          <span className="font-bold">@harshit % </span>
+          show tech stack
         </p>
 
         <div className="label">
@@ -22,13 +23,15 @@ const Terminal = () => {
         </div>
 
         <ul className="content">
-          {techStack.map(({ category, items }) =>(
-            <li className="flex items-center">
+          {techStack.map(({ category, items }) => (
+            <li key={category} className="flex items-center">
               <Check className="check" size={20} />
               <h3>{category}</h3>
               <ul>
                 {items.map((item, i) => (
-                  <li key={i}>{item} {i < items.length - 1 ? "," : ""}</li>
+                  <li key={i}>
+                    {item} {i < items.length - 1 ? "," : ""}
+                  </li>
                 ))}
               </ul>
             </li>
